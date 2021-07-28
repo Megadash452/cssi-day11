2554297391483110154680fdeb2cabe98c4f9581
Martin Molina <marticraft680@gmail.com>
2021-07-28 16:44:04 +0000

    write note

--- starter/firebaseNotes/public/js/writeNote.js
+++ starter/firebaseNotes/public/js/writeNote.js
@@ -13,6 +13,16 @@ window.onload = e => {
 const handleNoteSubmit = () => {
     console.log("note submission function called");
     // 1. Capture the form data
+    let note = {
+        title: document.getElementById('noteTitle').value,
+        text: document.getElementById('noteText').value
+    };
+    console.log(note);
     // 2. Format the data and write it to our database
+    firebase.database().ref(`users/${googleUser.uid}`)
+        .push(note).then(() => {
+            document.getElementById('noteTitle').value = "";
+            document.getElementById('noteText').value = "";
+        });
     // 3. Clear the form so that we can write a new note
 }
